28862ecd50cccc1e862913648585f2f0de69cebe
Jarrod Bogard <jarrodbogard@gmail.com>
2022-07-11 11:41:05 -0500

    changed dotenv.config()

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-// if(process.env.NODE_ENV !== "production") {
-//     require("dotenv").config()
-// }
-require("dotenv").config()
+if(process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
 
 const express = require("express");
 const app = express();

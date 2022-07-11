1362c65f7299c0cc41492d333a58cec12b9e40fa
Jarrod Bogard <jarrodbogard@gmail.com>
2022-07-11 12:13:02 -0500

    switched app.set views on line 16/17

--- server.js
+++ server.js
@@ -7,15 +7,13 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 3000
-const path = require('path')
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-// app.set("layout", "layouts/layout");
-app.set("views", path.join(__dirname, "views"));
+app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }))

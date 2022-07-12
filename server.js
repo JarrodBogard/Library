d5c7be32fac72046e37f54e91ecca40382ab8b82
Jarrod Bogard <jarrodbogard@gmail.com>
2022-07-11 20:01:15 -0500

    added book model/routes/view

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -26,5 +27,6 @@ db.once("open", () => console.log("Connected to Mongoose"))
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
-app.listen(PORT || 3000);
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
